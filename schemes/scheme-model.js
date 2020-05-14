617d622044a5e10a4e86783d7e369ca92b77204c
Harrison <hmseaborn@gmail.com>
2020-05-14 07:45:09 -0700

    smores done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -17,8 +17,43 @@ function find() {
 function findById(id) {
   return db("schemes").where("id", id).first();
 }
-function findSteps() {}
-function add() {}
-function update() {}
-function remove() {}
-function addStep() {}
+function findSteps(id) {
+  return db("steps")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .join("schemes as schemes", "steps.scheme_id", "schemes.id")
+    .where("schemes.id", id)
+    .orderBy("steps.step_number", "asc");
+}
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, "id")
+    .then((ids) => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+function update(changes, id) {
+  return db("schemes")
+    .update(changes)
+    .where({ id })
+    .then(() => {
+      return findById(id);
+    });
+}
+function remove(id) {
+  return db("schemes").del().where({ id });
+}
+function addStep(step, schemeId) {
+  step.scheme_id = schemeId;
+  return db("steps")
+    .insert(step, "id")
+    .then((ids) => {
+      const [id] = ids;
+      return db("steps").where({ id }).first();
+    });
+}
